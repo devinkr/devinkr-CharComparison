3bee65e59fcaf503950c28d7b3ebceed8f1f4abf
Devin Raleigh <devinkr@gmail.com>
2023-02-22 21:43:52 +0000

    Char comparison

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,6 +11,19 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
+        int i = 0;
+        while (i < a.length && i < b.length) {
+            int aCode = (int)Character.toLowerCase(a[i]);
+            int bCode = (int)Character.toLowerCase(b[i]);
+            if (aCode == bCode) {
+                i++;
+                continue;
+            }
+            if (aCode < bCode) return -1;
+            else {
+                return 1;
+            }
+        }
         return 0;
     }
 }
